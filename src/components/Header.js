ac1d762efbb6bfa2363ac6fa1404afb3da3e3923
jelenkoo10 <veljkojelenkovic00@gmail.com>
2025-03-17 23:10:41 +0100

    [REFACTOR] Updating Header display

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,45 @@
-import { useState, useRef } from "react";
-import { gsap } from "gsap";
-import HelloSvg from "../images/illustrations/hello.svg";
-import GitSvg from "../images/icons/git.svg";
-import ReactSvg from "../images/icons/react.svg";
-import CssSvg from "../images/icons/css.svg";
+import ProgrammingIllustration from "../images/illustrations/programming-setup.svg";
 
 function Header() {
   return (
     <header>
-      <section className="flex justify-between items-center p-5" id="home">
+      <section
+        className="flex justify-between items-center p-6 lg:max-w-[1200px] lg:mx-auto"
+        id="home"
+      >
         <img
           src={require("../images/icons/welcome.png")}
           alt="Icon that says `welcome`"
-          className="w-20"
+          className="w-20 lg:w-24"
         />
-        <nav></nav>
+        <nav className="flex items-center justify-end gap-4 pt-1 md:gap-8">
+          <a
+            href="#projects"
+            className="text-[var(--base-color)] font-bold leading-none md:text-lg"
+          >
+            Projects
+          </a>
+          <a
+            href="#contact"
+            className="text-[var(--base-color)] font-bold leading-none md:text-lg"
+          >
+            Contact
+          </a>
+        </nav>
       </section>
 
-      <section className="header-illustration">
-        <div className="illustration">
-          <img src={HelloSvg} alt="`Hello` illustration" className="hello" />
-          <img src={GitSvg} alt="Git icon" className="git-icon" />
-          <img src={ReactSvg} alt="React icon" className="react-icon" />
-          <img src={CssSvg} alt="CSS icon" className="css-icon" />
+      <section className="flex flex-col gap-8 p-6 mb-8 lg:flex-row lg:items-center lg:w-[800px] lg:gap-12 lg:mx-auto">
+        <div className="text-center lg:order-2 lg:text-left">
+          <h1 className="text-2xl font-bold mb-2">
+            Hello world! My name is Veljko.
+          </h1>
+          <p>junior frontend developer | information technology student</p>
         </div>
-        <div className="introduction">
-          <h1 id="header-title">Hello world! I am Veljko.</h1>
-          <p className="header-desc">
-            frontend developer | software engineering student
-          </p>
-          <button className="header-btn">Let me introduce myself</button>
-        </div>
+        <img
+          src={ProgrammingIllustration}
+          alt="Illustration of technological devices"
+          className="max-w-[300px] mx-auto lg:order-1 lg:mx-0"
+        />
       </section>
     </header>
   );
