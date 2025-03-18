c14e9359eeed193730132bf9566fd222ed5cee68
jelenkoo10 <veljkojelenkovic00@gmail.com>
2025-03-18 12:50:39 +0100

    [REFACTOR] Adding responsive breakpoints in Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "300px",
+      md: "768px",
+      slg: "900px", // Semi-large
+      lg: "1024px",
+      xl: "1200px",
+      "2xl": "1500px",
+    },
   },
   plugins: [],
 };
